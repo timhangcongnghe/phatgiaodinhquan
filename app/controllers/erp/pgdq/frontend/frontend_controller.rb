ed04f8d6dc7ed8199a7bb433149da1bfdfcf6bcb
Nguyen Ngoc Son <kinhdoanh@timhangcongnghe.com>
2020-08-31 08:17:20 +0000

    update

---
+++ app/controllers/erp/pgdq/frontend/frontend_controller.rb
@@ -0,0 +1,25 @@
+require_dependency "erp/application_controller"
+module Erp::Frontend
+  class FrontendController < Erp::ApplicationController
+    include Erp::ApplicationHelper
+    before_action :set_view
+    before_action :redirect_subdomain
+
+    def redirect_subdomain
+      if request.host == 'www.phatgiaodinhquan.org'
+        redirect_to 'https://phatgiaodinhquan.org' + request.fullpath, :status => 301
+      end
+    end
+    
+    layout 'erp/frontend/index'
+    
+    def current_ability
+      @current_ability ||= Erp::Ability.new(current_user)
+    end
+    
+    private
+    def set_view
+      session[:current_view] = "frontend"
+    end
+  end
+end
