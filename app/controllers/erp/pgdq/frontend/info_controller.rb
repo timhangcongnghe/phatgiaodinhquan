4a13f409c2b43a7342239f02fd89b3735e42ad8a
Son Nguyen Ngoc <sonnn@hoangkhang.com.vn>
2020-09-24 08:36:21 +0700

    update

--- app/controllers/erp/pgdq/frontend/info_controller.rb
+++ app/controllers/erp/pgdq/frontend/info_controller.rb
@@ -4,6 +4,7 @@ module Erp
       class InfoController < Erp::Frontend::FrontendController
         def about
           @body_class = 'bp-nouveau post-template-default single single-post postid-1833 single-format-standard theme-jannah tie-no-js woocommerce-no-js wrapper-has-shadow block-head-1 magazine2 demo is-lazyload is-thumb-overlay-disabled is-desktop is-header-layout-3 has-header-ad sidebar-right has-sidebar post-layout-1 narrow-title-narrow-media is-standard-format has-mobile-share hide_breaking_news no-js'
+          @meta_description = 'Do có lịch sử lâu dài và địa hình, cũng như sự đa dạng của các tộc người vì vậy Phật giáo tại huyện Định Quán cũng rất đa dạng và phong phú.'
         end
         
         def member
